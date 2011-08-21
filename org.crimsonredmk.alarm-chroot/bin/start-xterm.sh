32cb6d0714b233063c427f6df1c84f0ee51d3632
Mike Staszel <crimsonredmk@gmail.com>
2011-08-21 14:32:25 -0500

    Renamed to org.crimsonredmk.alarm-chroot

---
+++ org.crimsonredmk.alarm-chroot/bin/start-xterm.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NAME=alarm-chroot
+
+APPID=org.crimsonredmk.${NAME}
+
+APPDIR=/media/cryptofs/apps/usr/palm/applications/${APPID}
+
+XTERM=/media/cryptofs/apps/usr/palm/applications/org.webosinternals.xterm/bin/xterm
+
+${XTERM} -display :0.0 -maximize -e "sh -x ${APPDIR}/bin/chroot-wrapper.sh" &
